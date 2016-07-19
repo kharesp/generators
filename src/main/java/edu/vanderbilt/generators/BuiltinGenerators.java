67cfca2386fc08a85565e8e6630fe30ec3b98866
kharesp <shweta.p.khare@gmail.com>
2016-07-19 18:06:22 +0000

    changes for milestone#1

--- src/main/java/edu/vanderbilt/generators/BuiltinGenerators.java
+++ src/main/java/edu/vanderbilt/generators/BuiltinGenerators.java
@@ -1,5 +1,5 @@
 package edu.vanderbilt.generators;
-
+import java.util.function.Supplier;
 import java.util.Random;
 /**
  * Created by kharesp on 7/6/16.
@@ -9,6 +9,7 @@ public final class BuiltinGenerators {
     public static Generator<Integer> intGen() {
         return new Generator<Integer>(){
             Random random = new Random();
+            @Override
             Integer generate(){
                 return random.nextInt();
             }
@@ -16,15 +17,52 @@ public final class BuiltinGenerators {
     }
     public static Generator<Byte> byteGen(){
         return new Generator<Byte>(){
-            Random random= new Random();
-            Byte generate()
-            {
-                return (byte)random.nextInt();
+            Generator<Integer> integers = intGen();
+            @Override
+            Byte generate(){
+                return integers.generate().byteValue();
             }
         };
     }
+    public static <T> Generator<T> newGen(Supplier<T> generator){
+        return new Generator<T>() {
+            @Override
+            T generate() {
+                return generator.get();
+            }
+        };
+    }
+    public static Generator<Integer> rangeGen(int lower, int upper) {
+        System.out.println("lower:" + lower + "higher:"+ upper);
+        Generator<Integer> integers = intGen();
+        return newGen(() -> lower + Math.abs(integers.generate()) % (upper-lower));
+    }
+    public static Generator<Character> charGen(){
+        Generator<Integer> integers = rangeGen(Character.MIN_VALUE, Character.MAX_VALUE);
+        return newGen(() -> (char)integers.generate().intValue());
+    }
+    public static Generator<Boolean> booleanGen(){
+        Generator<Integer> integers = intGen();
+        return newGen(() -> integers.generate() > 0);
+    }
+    public static Generator<Short> shortGen(){
+        Generator<Integer> integers = intGen();
+        return newGen(() -> integers.generate().shortValue());
+    }
+    public static Generator<Long> longGen(){
+        Generator<Integer> integers = intGen();
+        return newGen(() -> integers.generate().longValue());
+    }
+    public static Generator<Float> floatGen(){
+        Generator<Integer> integers = intGen();
+        return newGen(() -> integers.generate().floatValue());
+    }
+    public static Generator<Double> doubleGen(){
+        Generator<Integer> integers = intGen();
+        return newGen(() -> integers.generate().doubleValue());
+    }
     public static void main(String[] args){
-        System.out.println(intGen().generate());
-        System.out.println(byteGen().generate().intValue());
+        Generator<Character> test = charGen();
+        System.out.println(test.generate());
     }
 }
